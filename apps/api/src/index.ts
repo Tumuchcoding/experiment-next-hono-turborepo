43f1cd4baa211628ef5e3875b7fa8e9c0eea7e80
Tu Nguyen <joejack12345@gmail.com>
2025-09-21 20:55:25 -0700

    ori code

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,47 +1,47 @@
 // import { serve } from "@hono/node-server"
-// import { Hono } from "hono"
-// import { setCookie } from "hono/cookie"
-// import { sign } from "hono/jwt"
+import { Hono } from "hono"
+import { setCookie } from "hono/cookie"
+import { sign } from "hono/jwt"
 
-// export const EXPIRATION_TIME_IN_SECONDS = 31_536_000 // 1 year
+export const EXPIRATION_TIME_IN_SECONDS = 31_536_000 // 1 year
 
-// export const isDevelopment = process.env.NODE_ENV === "development"
+export const isDevelopment = process.env.NODE_ENV === "development"
 
-// const app = new Hono()
+const app = new Hono()
 
-// app.get("/", (c) => {
-//   return c.text("Hello Hono!")
-// })
+app.get("/", (c) => {
+  return c.text("Hello Hono!")
+})
 
-// app.get("/test", (c) => {
-//   return c.json({ data: "Hello Hono!" })
-// })
+app.get("/test", (c) => {
+  return c.json({ data: "Hello Hono!" })
+})
 
-// app.get("/auth", async (c) => {
-//   const token = await sign({}, "PRIVATE_KEY")
-//   setCookie(c, "session", token, {
-//     httpOnly: true,
-//     maxAge: EXPIRATION_TIME_IN_SECONDS,
-//     path: "/",
-//     priority: "medium",
-//     sameSite: "lax",
-//     secure: !isDevelopment,
-//   })
-//   return c.json({ ok: true })
-// })
+app.get("/auth", async (c) => {
+  const token = await sign({}, "PRIVATE_KEY")
+  setCookie(c, "session", token, {
+    httpOnly: true,
+    maxAge: EXPIRATION_TIME_IN_SECONDS,
+    path: "/",
+    priority: "medium",
+    sameSite: "lax",
+    secure: !isDevelopment,
+  })
+  return c.json({ ok: true })
+})
 
-// app.get("/auth/signin", async (c) => {
-//   const token = await sign({}, "PRIVATE_KEY")
-//   setCookie(c, "session", token, {
-//     httpOnly: true,
-//     maxAge: EXPIRATION_TIME_IN_SECONDS,
-//     path: "/",
-//     priority: "medium",
-//     sameSite: "lax",
-//     secure: !isDevelopment,
-//   })
-//   return c.json({ ok: true })
-// })
+app.get("/auth/signin", async (c) => {
+  const token = await sign({}, "PRIVATE_KEY")
+  setCookie(c, "session", token, {
+    httpOnly: true,
+    maxAge: EXPIRATION_TIME_IN_SECONDS,
+    path: "/",
+    priority: "medium",
+    sameSite: "lax",
+    secure: !isDevelopment,
+  })
+  return c.json({ ok: true })
+})
 
 // serve(
 //   {
@@ -53,17 +53,17 @@
 //   },
 // )
 
-import { Hono } from "hono"
+// import { Hono } from "hono"
 
-const app = new Hono()
+// const app = new Hono()
 
-const welcomeStrings = [
-  "Hello Hono!",
-  "To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/hono",
-]
+// const welcomeStrings = [
+//   "Hello Hono!",
+//   "To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/hono",
+// ]
 
-app.get("/", (c) => {
-  return c.text(welcomeStrings.join("\n\n"))
-})
+// app.get("/", (c) => {
+//   return c.text(welcomeStrings.join("\n\n"))
+// })
 
 export default app
