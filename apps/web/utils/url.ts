f5fbe9e48a5648821231515c6a3a671a604541c3
Tu Nguyen <joejack12345@gmail.com>
2025-10-02 20:43:36 -0700

    yr

--- apps/web/utils/url.ts
+++ apps/web/utils/url.ts
@@ -2,12 +2,12 @@ export const isDevelopment = process.env.NODE_ENV === "development"
 
 export const BASE_URL_WEB_HASHMAP = {
   DEVELOPMENT: "http://localhost:3001",
-  PRODUCTION: "https://experiment-next-hono-turborepo-web.vercel.app",
+  PRODUCTION: "https://experiment-next-hono-turborepo-web-ivory.vercel.app/",
 }
 
 export const BASE_URL_API_HASHMAP = {
   DEVELOPMENT: "http://localhost:3002",
-  PRODUCTION: "https://experiment-next-hono-turborepo-api.vercel.app",
+  PRODUCTION: "https://experiment-next-hono-turborepo-api-three.vercel.app/",
 }
 
 export const BASE_URL = {
