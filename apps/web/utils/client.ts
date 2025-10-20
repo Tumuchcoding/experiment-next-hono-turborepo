ddea8ad0334038b2fea518a5c72b344f440acb66
Tu Nguyen <joejack12345@gmail.com>
2025-10-19 18:04:43 -0700

    hh

--- apps/web/utils/client.ts
+++ apps/web/utils/client.ts
@@ -1,19 +1,19 @@
-import { createORPCClient, type InferClientContext } from "@orpc/client"
-import { RPCLink } from "@orpc/client/fetch"
-import type { ContractRouterClient } from "@orpc/contract"
-import type { AppContract } from "api"
-import { BASE_URL } from "@/utils/url"
+import { createORPCClient, type InferClientContext } from "@orpc/client";
+import { RPCLink } from "@orpc/client/fetch";
+import type { ContractRouterClient } from "@orpc/contract";
+import type { AppContract } from "@/orpc/contract";
+import { BASE_URL } from "@/utils/url";
 
-type ApiClient = ContractRouterClient<AppContract>
-type ApiClientContext = InferClientContext<ApiClient>
+type ApiClient = ContractRouterClient<AppContract>;
+type ApiClientContext = InferClientContext<ApiClient>;
 
 const rpcLink = new RPCLink<ApiClientContext>({
-  url: () => `${BASE_URL.API}/rpc`,
   fetch: (request, init, _options, _path, _input) =>
     fetch(request, {
       ...init,
       credentials: "include",
     }),
-})
+  url: () => `${BASE_URL.API}/rpc`,
+});
 
-export const client: ApiClient = createORPCClient<ApiClient>(rpcLink)
+export const client: ApiClient = createORPCClient<ApiClient>(rpcLink);
